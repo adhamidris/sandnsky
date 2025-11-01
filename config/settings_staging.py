c11936e38ef295c7c2ab183a90288a72451d9e6c
adhamidris <adhamidris@outlook.com>
2025-11-01 03:16:43 +0200

    update

--- config/settings_staging.py
+++ config/settings_staging.py
@@ -42,3 +42,18 @@ DEBUG_TOOLBAR_CONFIG = {
     "RESULTS_CACHE_SIZE": 3,
     "SHOW_TEMPLATE_CONTEXT": False,
 }
+
+DEBUG_TOOLBAR_PANELS = [
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    'debug_toolbar.panels.templates.TemplatesPanel',
+    'debug_toolbar.panels.cache.CachePanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.signals.SignalsPanel',
+    'debug_toolbar.panels.history.HistoryPanel',
+    'debug_toolbar.panels.profiling.ProfilingPanel',
+]
+
