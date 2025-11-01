1173be665cc458dd1f478088cfcd384e2b954162
adhamidris <adhamidris@outlook.com>
2025-11-01 05:25:09 +0200

    update

--- config/settings.py
+++ config/settings.py
@@ -94,6 +94,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -170,7 +171,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = "/home/adhamidris/static"
+STATIC_ROOT = BASE_DIR / 'staticfiles'            # collectstatic output
+
+STORAGES = locals().get("STORAGES", {})
+STORAGES["staticfiles"] = {
+    "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage"
+}
 
 # Media storage defaults (local filesystem)
 MEDIA_URL = '/media/'
