f6538c4fd0d7f65c433c1d3c4c44cb3c1c9c9305
adhamidris <adhamidris@outlook.com>
2025-11-01 05:35:41 +0200

    update

--- config/settings.py
+++ config/settings.py
@@ -127,11 +127,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        env="DATABASE_URL",
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,   # persistent connections
+        ssl_require=True,   # Render uses SSL
+    )
 }
 
 
