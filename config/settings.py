f522634988523f7f61121b71761d73d30a81d1b4
adhamidris <adhamidris@outlook.com>
2025-11-02 00:08:39 +0200

    db test

--- config/settings.py
+++ config/settings.py
@@ -128,10 +128,18 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+  "default": {
+    "ENGINE": "django.db.backends.mysql",
+    "NAME": "sandnskydb",
+    "USER": "sandnsky",
+    "PASSWORD": "sandnsky123",
+    "HOST": "adhamidris.mysql.pythonanywhere-services.com",     # e.g., yourusername.mysql.pythonanywhere-services.com
+    "PORT": "3306",
+    "OPTIONS": {
+      "charset": "utf8mb4",
+      "init_command": "SET sql_mode='STRICT_TRANS_TABLES'"
     }
+  }
 }
 
 
